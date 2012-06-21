5fa0b2802604d21d617cf3a65dfcd93a95259da1
Albert De La Fuente <vonpupp@gmail.com>
2012-06-21 12:25:40 -0300

    - Minor changes on paths & parameters

--- cmd.sh
+++ cmd.sh
@@ -1,2 +1,2 @@
-python wf2ea.py -p "/home/afu/siga-svn/PROJETO_SIGA/3.TEP - Consolidado/3. Wireframes" -r /home/afu/siga-svn/ -f "C:\\SIGA\\DesenvolvimentoSiga\\PROJETO_SIGA\\3.TEP - Consolidado\\3. Wireframes\\" -o ./out.csv -v 2 
+python wf2ea.py -p "/home/afu/siga-svn/PROJETO_SIGA/3.TEP - Consolidado/3. Wireframes" -r "/home/afu/siga-svn/PROJETO_SIGA/3.TEP - Consolidado/3. Wireframes/" -f "C:\\SIGA\\DesenvolvimentoSiga\\PROJETO_SIGA\\3.TEP - Consolidado\\3. Wireframes\\" -o ./out.csv -v 2 
 unix2dos out.csv
